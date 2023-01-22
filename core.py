3e436deea0b369d871abd52bc89311b0fbaf1a12
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2023-01-22 12:36:49 +0900

    upd

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import bpy
 
 from .register_class import _get_cls, operator
@@ -13,6 +15,18 @@ class CDO_OT_run_script(bpy.types.Operator):
     file: bpy.props.StringProperty() = bpy.props.StringProperty()  # type: ignore
 
     def execute(self, context):
+        file = Path(self.file)
+        if not file.is_file():
+            self.report({"WARNING"}, "Set file name.")
+            return {"CANCELLED"}
+        if txt := bpy.data.texts.get(file.name):
+            bpy.data.texts.remove(txt)
+        txt = bpy.data.texts.load(str(file))
+        if scr := bpy.data.screens.get("Scripting"):
+            areas = [a for a in scr.areas if a.type == "TEXT_EDITOR"]
+            if areas:
+                areas[0].spaces[0].text = txt
+        exec(file.read_text())
         return {"FINISHED"}
 
 
